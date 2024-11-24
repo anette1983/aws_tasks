2daed8cc483e7c1fcde625e6953c9b6665013055
Hanna K <anna_kon@bigmir.net>
2024-11-24 23:35:19 +0200

    add changes to the event processing

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -3,14 +3,19 @@ exports.handler = async (event) => {
 	const path = event.rawPath || '/';
 	const httpMethod = event.requestContext?.http?.method || 'UNKNOWN';
 
-	if (path !== '/hello' && httpMethod !== 'GET') {
-		return {
-			statusCode: 400,
-			message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
-		};
-	}
-	return {
+	const returnSuccessObject = {
+		statusCode: 400,
+		message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
+	};
+
+	const returnErrObject = {
 		statusCode: 200,
 		message: 'Hello from Lambda',
 	};
+
+	if (path !== '/hello' && httpMethod !== 'GET') {
+		return returnErrObject;
+	}
+
+	return returnSuccessObject;
 };
