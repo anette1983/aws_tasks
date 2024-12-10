7878d48be904951abca762b5f251fe2ffc49acfd
Hanna K <anna_kon@bigmir.net>
2024-12-10 20:28:29 +0200

    add changes to lambda

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,19 +1,32 @@
-const OpenMeteoSDK = require('/opt/nodejs/openMeteoSDK');
-
-const openMeteo = new OpenMeteoSDK('https://api.open-meteo.com/v1/forecast');
+const axios = require('axios');
 
 exports.handler = async (event) => {
 	try {
 		const latitude = 52.52;
 		const longitude = 13.41;
+		const baseURL = 'https://api.open-meteo.com/v1/forecast';
 
-		const weatherData = await openMeteo.getWeatherForecast(latitude, longitude);
+		const response = await axios.get(baseURL, {
+			params: {
+				latitude,
+				longitude,
+				current: 'temperature_2m,wind_speed_10m',
+				hourly: 'temperature_2m,relative_humidity_2m,wind_speed_10m',
+			},
+		});
+
+		const weatherData = response.data;
+
+		// if (!weatherData.hourly) {
+		// 	throw new Error('The hourly field is missing in the API response');
+		// }
 
 		return {
 			statusCode: 200,
 			body: JSON.stringify(weatherData),
 		};
 	} catch (error) {
+		console.error('Error in Lambda:', error);
 		return {
 			statusCode: 500,
 			body: JSON.stringify({ error: error.message }),
